8648f6a0b9acec2d95e63c647237641f658767eb
Helios <hien.dnnt@gmail.com>
2020-07-13 17:10:45 +0700

    create message controller

---
+++ server/controllers/message.controller.js
@@ -0,0 +1,51 @@
+const UserRoom = require('../models/userRoom.model');
+const User = require('../models/user.model');
+const Room = require('../models/room.model');
+const Message = require('../models/message.model');
+const ObjectId = require('mongoose').Types.ObjectId; 
+const jwt = require('jsonwebtoken');
+
+module.exports.getAllMessage = async (req, res) => {
+  try {
+    const token = req.header('x-access-token');
+    const tokenVerify = jwt.verify(token, process.env.SECRET_KEY);
+    const { userId } = tokenVerify;
+    const user = await User.find({_id: ObjectId(userId)});
+    if(!user) {
+      res.json({ 
+        success: false    
+    });
+      return;
+    }
+    console.log(userId)
+    const dataRoomOfUser = await UserRoom.find({user: ObjectId(userId)})
+      .populate({
+        path: "room",
+        select: "name messages _id",
+        populate: {
+          path: "messages",
+          select: "_id user content",
+          populate: {
+            path: "user",
+            select: "_id name avatarUrl"
+          }
+        }
+      })
+      .exec();
+    const allMessageOfRoomOfUser = {};
+    dataRoomOfUser.forEach(item => {
+      allMessageOfRoomOfUser[item.room._id] = item.room;
+    })
+    res.json({
+      allMessage: allMessageOfRoomOfUser,
+      success: true
+    });
+  } catch(e) {
+    res.json({ 
+      success: false,
+      msg: e
+    });
+    console.log(e);
+  }
+
+}
