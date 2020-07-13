7e76749f88c8fc4943161f60240233bbb6f4aedb
Helios <hien.dnnt@gmail.com>
2020-07-13 20:37:19 +0700

    fix index in server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const { allowedNodeEnvironmentFlags } = require('process');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const port = process.env.PORT || 6969;
+require('./controllers/socketIo.controller')(io);
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const authApi = require('./routes/auth.route');
+const dataApi = require('./routes/message.route');
+
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) 
 app.use(express.static("public"));
+
+app.use('/auth', authApi);
+app.use('/data', dataApi);
 
 app.get("/", (req, res) => {
   res.send("connection complete");
@@ -15,4 +31,4 @@ app.get("/", (req, res) => {
 server.listen(port, () => {
   console.log(`Server listen at localhost:${port}`);
 } );
-
+ 
